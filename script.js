e69262947acccd4f18eb96988fb3d085ab6fc22a
LauriL <bhy890@myy.haaga-helia.fi>
2025-11-14 16:25:23 +0200

    indexing of boxes and setCurrentDay function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //.js koodit
+function setCurrentDay() {
+    const today = new Date();
+    const day = today.getDate();
+    const dayString = "luukku" + day.toString();
+    
+    document.getElementById(dayString).style.backgroundColor = "green";
+}
+setCurrentDay();
