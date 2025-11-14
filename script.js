d9fd06bcb6412d491a851ecb65754f5b5ad8fed6
LauriL <bhy890@myy.haaga-helia.fi>
2025-11-14 17:55:01 +0200

    Grid-container structure for the boxes, more functions for box manipulation in relation to current Day

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-//.js koodit
+//sivuston lataamisen yhteydessä ajettavat funktiot kerätty tämän funktion sisään
 function setCurrentDay() {
     const today = new Date();
     const day = today.getDate();
-    const dayString = "luukku" + day.toString();
-    
+
+    colourCurrentDayBox(day);
+    openOldBoxes(day);
+}
+
+//Värittää nykyisen päivän luukun vihreäksi
+function colourCurrentDayBox(day) {
+    const dayString = "box" + day.toString();
+
     document.getElementById(dayString).style.backgroundColor = "green";
 }
-setCurrentDay();
+
+//Avaa menneiden päivien luukut (Nyt vasta tosin muuttaa niiden väriä, TBD)
+function openOldBoxes(day) {
+    for (let i = 1; i < day; i++) {
+        const dayString = "box" + i.toString();
+        document.getElementById(dayString).style.backgroundColor = "lightgreen";
+    }
+    if (day <= 24 && day >= 1) {
+        document.getElementById("box30").style.backgroundColor = "lightgreen";
+    }
+}
+
