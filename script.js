a647c2fa3b11b5c1e6ef3d54b5daf0206e4721e8
bhy890@myy.haaga-helia.fi <bhy890@myy.haaga-helia.fi>
2025-11-21 17:38:26 +0200

    Text to luukut 18-24

--- script.js
+++ script.js
@@ -18,18 +18,52 @@ luukut[14] = "Spicy Fifty";
 luukut[15] = "Moscow Mule";
 luukut[16] = "Daiquiri";
 luukut[17] = "Negroni";
-luukut[18] = "Cosmopolitan";
-luukut[19] = "Irish Coffee";
+
+luukut[18] = "18.12 <br> <b>Cosmopolitan <br> ainekset:</b> <br> 3cl sitruunanmakuista vodkaa <br> 1cl triple sec <br> 3cl karpalomehua <br> 1cl limetin mehua <br> <b>valmistusohjeet:</b> <br> \
+            1. Mittaa sitruunavodka, triple sec -likööri, <br>karpalomehu ja tuorepuristettu limetinmehu <br>shakerin metalliosaan. <br> \
+            2. Täytä shaker jäillä, sulje kansi<br> ja ravista 15 sekuntia,<br> kunnes shakerin pinta tuntuu kylmältä. <br>  \
+            3. Avaa shaker ja siivilöi juoma<br> jäähdytettyyn lasiin. <br> \
+            4. Koristele Cosmopolitan limetinlohkolla<br> tai sitruunan- tai appelsiininkuorisuikaleella. \
+            <img src=\"pictures/Cosmopolitan.jpg\">";
+
+luukut[19] = "19.12 <br> <b> Irish Coffee <br> ainekset: </b> <br> 4dl vahvaa, kuumaa kahvia <br> 2rkl fariinisokeria <br> 8cl irlantilaista viskiä <br> 1dl kuohukermaa <br> <b>valmistusohjeet:</b> <br> \
+            1. Keitä kahvi. Vatkaa kerma löysähköksi vaahdoksi. <br> \
+            2. Lämmitä mukeja kuumalla vedellä. Kaada vesi pois ja täytä mukista noin 3/4 kahvilla. Lisää kuuman kahvin joukkoon sokeri ja sekoita, kunnes sokeri on täysin sulanut kahviin. <br> \
+            3. Lisää sitten irlantilainen viski ja sekoita juoma. <br> \
+            4. Viimeistele kahvijuoma kermavaahdolla. Jos käytät kevyesti vatkattua kermaa, kaada kermaa rauhallisesti lusikan kautta kahvin päälle. \
+            <img src=\"pictures/irishCoffee.jpg\">";
+
 luukut[20] = "20.12 <br> <b>Espresso Martini</b> <br> <b>ainekset:</b> <br> 3cl vodkaa <br> 3cl kahvilikööriä <br> \
             3cl tuorettaa espressoa <br> (maun mukaan sokeria tai sokerisiirappia) <br> \
             koristeeksi kolme kahvipapua <br> <b>valmistusohjeet:</b> <br> 1. Mittaa cocktailshakeriin, <br> 3cl vodkaa, <br> 3cl kahvilikööriä ja 3cl tuoretta espressoa \
             <br> 2. Sekoita. <br> Halutessasi voit lisätä mukaan hieman <br> sokeria tai sokerisiirappia <br> 3. Lisää mukaan isoja jääpaloja. Sulje shaker <br> ja ravistele \
             voimakkaasti noin 15 sekuntia. <br> Kun shakerin piinta kylmenee, juoma on valmis. <br> 4. Siivilöi juoma tarjoilulasiin <br> ja koristele kolmella kahvipavulla \
-            ";
-luukut[21] = "Hugo";
-luukut[22] = "Limoncello spritz";
-luukut[23] = "Valkovenäläinen";
-luukut[24] = "Sex on the beach"
+            <img src=\"pictures/espressoMartini.jpg\">";
+
+luukut[21] = "21.12 <br> <b>Hugo <br> ainekset:</b> <br> 2rkl tuoretta minttua <br> 2cl seljankukkamehutiivistettä <br> 10cl kuohuviiniä <br> jäitä <br> <b>valmistusohjeet:</b> <br> \
+            1. Laita isohkon viinilasin pohjalle jääpaloja, mintunlehdet sekä limetin siivu. <br> \
+            2. Mittaa lasiin seljankukkamehutiiviste. <br> \
+            3. Kaada päälle Prosecco. <br> \
+            4. Pidennä oman maun mukaan tilkalla kivennäisvettä. \
+            <img src=\"pictures/Hugo.jpg\">";
+            
+luukut[22] = "22.12 <br> <b>Limoncello spritz <br> ainekset:</b> <br> 1/2dl kuohuviiniä <br> 1/2dl limoncelloa <br> 1/2dl kivennäisvettä <br> sitruunaviipale <br> jäitä <br> <b>valmistusohje:</b> <br> \
+            1. Kaada isoon viinilasiin kuohuviini ja sen jälkeen Limoncello likööri. <br> \
+            2. Lisää lasiin runsaasti jäitä ja kivennäisvettä. <br> \
+            3. Lisää lasiin sitruunaviipale. \
+            <img src=\"pictures/limoncelloSpritz.jpg\">";
+
+luukut[23] = "23.12 <br> <b>Valkovenäläinen <br> ainekset:</b> <br> 3cl vodkaa <br> 3cl kahvilikööriä <br> 1/2dl kermaa tai vastaavaa <br> jäitä <br> <b>valmistusohje:</b> <br> \
+            1. Lisää jäät lasiin. <br> \
+            2. Kaada sekaan vodka ja kahvilikööri. <br> \
+            3. Kaada kerma lasiin varovasti lusikan kautta. Älä sekoita. \
+            <img src=\"pictures/Valkovenäläinen.jpg\">";
+
+luukut[24] = "24.12 <br> <b>Sex on the beach <br> ainekset:</b> <br> 2cl vodkaa <br> 2cl persikkalikööriä <br> 4cl karpalomehua <br> 8cl appelsiinimehua <br> jäitä <br> <b>valmistusohje:</b> <br> \
+            1. Kaada vodka, persikkalikööri, karpalomehu, ja appelsiinimehu jääpaloilla täytettyyn shakeriin. Ravista. <br> \
+            2. Kaada juoma ja jäät lasiin. <br> \
+            3. Koristele halutessasi esimerkiksi appelsiiniviipaleella. \
+            <img src=\"pictures/sexOnTheBeach.jpg\">";
 
 function playAudio(){
     let music = document.getElementById("jingle_Bells");
