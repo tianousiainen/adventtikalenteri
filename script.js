dfeb9b06f00b466fe70aa6215181ca8ad43c7c25
bhy845@myy.haaga-helia.fi <bhy845@myy.haaga-helia.fi>
2025-11-21 17:44:28 +0200

    Luukut 0-7 js-tiedostossa lisätty niiden ainesosat ja valmistusohjeet.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 let luukut = [];
-luukut[0] = "Whisky Sour <br> valmistusaineet: <br> 4cl viskiä <br> 3cl sitruunamehu <br> \
-             1cl sokerisiirappia <br> 2cl munanvalkuaista <br> jäitä <br> appelsiinin kuorta";
-luukut[1] = "Gin tonic";
-luukut[2] = "Mojito";
-luukut[3] = "Manhattan";
-luukut[4] = "Margarita";
-luukut[5] = "Long Island Iced Tea";
-luukut[6] = "Kir Royal";
-luukut[7] = "Champagne Sour";
+luukut[0] = "30.11 <br> <b>Whisky Sour</b> <br> <b>Ainekset:</b> <br> 4cl viskiä <br> 3cl sitruunamehua <br> \
+             1cl sokerisiirappia <br> 2cl munanvalkuaista <br> Jäitä <br> Appelsiinin kuorta <br> \
+             <b>Valmistusohjeet:</b> <br> 1. Mittaa viski, sitruunamehu, sokerisiirappi <br> ja munanvalkuainen shakeriin. <br> \
+             2. Lisää jäät ja ravista 30-60 sekuntia. <br> Siivilöi lasiin. <br> 3. Lisää jäät ja koristele appelsiinin kuorella.";
+luukut[1] = "1.12 <br> <b>Gin tonic</b> <br> <b>Ainekset:</b> <br> Runsaasti jäitä <br> 1 osa giniä <br> 2 osaa tonic vettä <br> \
+            Koristeeksi sitruunan -tai limenviipaleita <br> ja rosmariinin oksia <br> \
+            <b>Valmistusohjeet:</b> <br> 1. Täytä lasi jäillä. <br> 2. Lisää lasiin 1 osa giniä ja 2 osaa tonic-vettä. <br> \
+            3. Sekoita hieman. <br> 4. Koristele juoma.";
+luukut[2] = "2.12 <br> <b>Mojito</b> <br> <b>Ainekset:</b> <br> 1/2 lime lohkoina <br> 2 tl ruokosokeria <br> \
+            10 kpl mintunlehtiä <br> 4 cl vaalea rommia <br> 6 cl soodavettä <br> Jäitä <br> Koristeeksi limen viipaleita <br> \
+            <b>Valmistusohjeet:</b> <br> 1. Murskaa limen lohkot ja sokeri lasiin. <br> 2. Hiero mintunlehtiä kevyesti käsissä <br> \
+            ja lisää ne lasiin. <br> 3. Lisää lasiin runsaasti jäitä. <br> Mittaa lasiin vaalea rommi ja soodavesi. <br> Sekoita varovasti. <br> \
+            5. Koristele limellä ja mintulla.";
+luukut[3] = "3.12 <br> <b>Manhattan</b> <br> <b>Ainekset:</b> <br> 3 cl amerikkalaista viskiä <br> 2 cl makeaa punaista vermuttia <br> \
+            1 tippa angosturaa <br> Koristeeksi cocktailkirsikka <br> <b>Valmistusohjeet:</b> <br> 1. Ravista nopeasti lasissa, jossa on jäämurskaa. <br> \
+            2. Kaada siivilöimättä cocktaillasiin. <br> 3. Koristele cocktailkirsikalla.";
+luukut[4] = "4.12 <br> <b>Margarita</b> <br> <b>Ainekset:</b> <br> 2 1/2 cl tequilaa <br> 1 1/2 cl triple sec -likööriä <br> \
+            2 cl limettimehua <br> Jäitä <br> Suolaa <br> <b>Valmistusohjeet:</b> <br> 1. Viilennä lasi jääkaapissa. <br> \
+            2. Tee halutessasi lasiin suolareunus.<br> Kostuta lasin reunus limentin mehulla <br> \
+            ja sen jälkeen pyöräytä lasia lautasella, <br> jossa on suolaa. <br> \
+            3. Lisää tequila, triple sec -likööri, limettimehu <br> ja muutama jääpala cocktail-shakeriin <br> \
+            tai kannelliseen astiaan. <br> Sekoita voimakkaasti. <br> 4. Kaada juoma siivilän läpi jäillä täytettyyn lasiin.";
+luukut[5] = "5.12 <br> <b>Long Island Iced Tea</b> <br> <b>Ainekset:</b> <br> 1,5 cl vodka <br> 1,5 cl tequila <br> \
+            1,5 cl vaalea rommi <br> 1,5 cl gini <br> 1,5 cl triple sec -likööriä <br> 3cl sitruunamehua <br> \
+            2 cl sokerisiirappia <br> 4 cl Coca-Colaa <br> Sitruunasiivu koristeeksi <br> <b>Valmistusohjeet:</b> <br> \
+            1. Mittaa cocktailshakeriin vodka, tequila, <br> vaalea rommi, gini ja triple sec -likööri. <br> \
+            2. Lisää sitruunamehu ja sokerisiirappi. <br> 3. Täytä shaker jäillä, sulje kansi <br> ja ravista 10-15 sekuntia. <br> \
+            4. Siivilöi juoma jäillä täytettyyn lasiin <br> ja lisää Coca-Cola. <br> 5. Koristele limelohkolla ja sekoita varovasti<br>";
+luukut[6] = "6.12 <br> <b>Kir Royal</b> <br> <b>Ainekset:</b> <br> 2 cl mustaherukka- tai marjalikööriä <br> \
+            10 cl kuohuviiniä <br> 2-3 vadelmaa <br> <b>Valmistusohjeet:</b> <br> 1. Mittaa marjalikööri lasin pohjalle <br> \
+            ja lisää vadelmat. <br> 2. Kaada kylmä kuohuviini päälle. <br> Tarjoa esimerkiksi alkumaljana.";
+luukut[7] = "7.12 <br> <b>Champagne Sour</b> <br> <b>Ainekset:</b> <br> 1-2 cl sitruunamehua <br> 1 tl sokeria <br> \
+            10 cl samppanjaa <br> Pieni sitruuna- tai appelsiinisiivu <br> <b>Valmistusohjeet</b> <br> \
+            1. Sekoita sitruunamehu ja sokeri. <br> Kaada seos kuohuviinilasin pohjalle. <br> \
+            2. Täytä lasi hitaasti samppanjalla <br> ja sekoita lusikalla. <br> \
+            3. Lisää koristeeksi pieni siivu sitruunaa <br> tai appelsiinia.";
 luukut[8] = "Mimosa";
 luukut[9] = "Bellini";
 luukut[10] = "Rommikola";
@@ -20,9 +47,9 @@ luukut[16] = "Daiquiri";
 luukut[17] = "Negroni";
 luukut[18] = "Cosmopolitan";
 luukut[19] = "Irish Coffee";
-luukut[20] = "20.12 <br> <b>Espresso Martini</b> <br> <b>ainekset:</b> <br> 3cl vodkaa <br> 3cl kahvilikööriä <br> \
+luukut[20] = "20.12 <br> <b>Espresso Martini</b> <br> <b>Ainekset:</b> <br> 3cl vodkaa <br> 3cl kahvilikööriä <br> \
             3cl tuorettaa espressoa <br> (maun mukaan sokeria tai sokerisiirappia) <br> \
-            koristeeksi kolme kahvipapua <br> <b>valmistusohjeet:</b> <br> 1. Mittaa cocktailshakeriin, <br> 3cl vodkaa, <br> 3cl kahvilikööriä ja 3cl tuoretta espressoa \
+            koristeeksi kolme kahvipapua <br> <b>Valmistusohjeet:</b> <br> 1. Mittaa cocktailshakeriin, <br> 3cl vodkaa, <br> 3cl kahvilikööriä ja 3cl tuoretta espressoa \
             <br> 2. Sekoita. <br> Halutessasi voit lisätä mukaan hieman <br> sokeria tai sokerisiirappia <br> 3. Lisää mukaan isoja jääpaloja. Sulje shaker <br> ja ravistele \
             voimakkaasti noin 15 sekuntia. <br> Kun shakerin piinta kylmenee, juoma on valmis. <br> 4. Siivilöi juoma tarjoilulasiin <br> ja koristele kolmella kahvipavulla \
             ";
